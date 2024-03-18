3abca6fde63bc4371dfc881f1b1dca0130562582
Jesus Garcia <jesus.garcia@open2000.cl>
2024-03-18 18:09:20 -0300

    (feat) se agrega el reto de las tablas y se desacopla la seguridad

---
+++ DDL-ROLLBACK.sql
@@ -0,0 +1,27 @@
+DROP TABLE public.broker_company CASCADE;
+
+DROP TABLE public.client CASCADE;
+
+DROP TABLE public.company CASCADE;
+
+DROP TABLE public.company_realtor CASCADE;
+
+DROP TABLE public.company_realtor_operation CASCADE;
+
+DROP TABLE public.letter_config CASCADE;
+
+DROP TABLE public.letter_fixdata CASCADE;
+
+DROP TABLE public.loan_client CASCADE;
+
+DROP TABLE public.loan_client_operation CASCADE;
+
+DROP TABLE public.loan_officer CASCADE;
+
+DROP TABLE public.operation CASCADE;
+
+DROP TABLE public.profile CASCADE;
+
+DROP TABLE public.realtor CASCADE;
+
+DROP TABLE public.users CASCADE;
--- DDL.sql
+++ DDL.sql
@@ -8,11 +8,11 @@ CREATE TABLE public.profile (
 
 CREATE TABLE public.users (
 	id serial NOT NULL,
-	rut varchar(50) NOT NULL,
+	social_security_number varchar(50) NULL,
 	names varchar(50) NOT NULL,
 	middle_name varchar(50) NOT NULL,
 	last_name varchar(50) NULL,
-	mail varchar(50) NULL,
+	mail varchar(50) NOT NULL,
 	business_position varchar(50) NULL,
 	username varchar(200) NOT NULL,
 	pass varchar(200) NOT NULL,
@@ -21,3 +21,153 @@ CREATE TABLE public.users (
 );
 
 alter table public.users add constraint fk_users_profile foreign key (id_profile)  references public.profile(id);
+
+
+-- business model
+
+
+CREATE TABLE public.broker_company (
+	id int8 NOT NULL,
+	"name" varchar(40) NOT NULL,
+	phone varchar(50) NOT NULL,
+	physical_add varchar(50) NOT NULL,
+	web_site varchar(500) NULL,
+	primary key (id)
+);
+
+CREATE TABLE public.client (
+	id int8 NOT NULL,
+	cellphone varchar(40) NOT NULL,
+	email varchar(50) NOT NULL,
+	last_name varchar(40) NOT NULL,
+	mailing_add varchar(500) NULL,
+	"name" varchar(40) NOT NULL,
+	primary key (id)
+);
+
+CREATE TABLE public.company (
+	id int8 NOT NULL,
+	mailing_add varchar(50) NOT NULL,
+	"name" varchar(36) NOT NULL,
+	nmls varchar(500) NOT NULL,
+	phisical_add varchar(50) NOT NULL,
+	primary key (id)
+);
+
+CREATE TABLE public.letter_fixdata (
+	id int8 NOT NULL,
+	conditions varchar(500) NOT NULL,
+	deleted bool NOT NULL,
+	finaltext varchar(500) NOT NULL,
+	subject varchar(500) NOT NULL,
+	primary key (id)
+);
+
+CREATE TABLE public.operation (
+	id int8 NOT NULL,
+	"name" varchar(36) NOT NULL,
+	primary key (id)
+);
+
+CREATE TABLE public.letter_config (
+	id int8 NOT NULL,
+	active bool NULL,
+	deleted bool NULL,
+	hoa float8 NOT NULL,
+	insurance float8 NOT NULL,
+	interest float8 NOT NULL,
+	loan_amount float8 NULL,
+	loan_term int4 NOT NULL,
+	loan_type varchar(500) NOT NULL,
+	"location" varchar(500) NOT NULL,
+	ltv float8 NOT NULL,
+	max_pay float8 NOT NULL,
+	mi float8 NULL,
+	price int4 NOT NULL,
+	taxes float8 NOT NULL,
+	unique_key varchar(250) NULL,
+	letter_fix_data_id int8 NULL,
+	operation_id int8 NULL,
+	primary key (id),
+	CONSTRAINT letter_config_hoa_check CHECK ((hoa >= (0)::double precision)),
+	CONSTRAINT letter_config_insurance_check CHECK ((insurance >= (0)::double precision)),
+	CONSTRAINT letter_config_interest_check CHECK ((interest >= (0)::double precision)),
+	CONSTRAINT letter_config_ltv_check CHECK ((ltv >= (0)::double precision)),
+	CONSTRAINT letter_config_max_pay_check CHECK ((max_pay >= (0)::double precision)),
+	CONSTRAINT letter_config_mi_check CHECK ((mi >= (0)::double precision)),
+	CONSTRAINT letter_config_taxes_check CHECK ((taxes >= (0)::double precision))
+);
+
+alter table public.letter_config add constraint fk_letter_config_operation foreign key (operation_id)  references public.operation(id);
+alter table public.letter_config add constraint fk_letter_config_letter_fix_data foreign key (letter_fix_data_id)  references public.letter_fixdata(id);
+
+CREATE TABLE public.loan_officer (
+	id int8 NOT NULL,
+	cellphone varchar(20) NOT NULL,
+	email varchar(50) NOT NULL,
+	last_name varchar(36) NOT NULL,
+	mailing_add varchar(75) NOT NULL,
+	"name" varchar(36) NOT NULL,
+	nmls varchar(50) NOT NULL,
+	company_id int8 NULL,
+	primary key (id)
+);
+alter table public.loan_officer add constraint fk_loan_officer_company foreign key (company_id)  references public.company(id);
+
+CREATE TABLE public.realtor (
+	id int8 NOT NULL,
+	cellphone varchar(20) NOT NULL,
+	email varchar(50) NOT NULL,
+	last_name varchar(40) NOT NULL,
+	license_number varchar(20) NOT NULL,
+	mailing_add varchar(500) NULL,
+	"name" varchar(40) NOT NULL,
+	notes varchar(500) NOT NULL,
+	broker_company_id int8 NULL,
+	primary key (id)
+);
+alter table public.realtor add constraint fk_realtor_broker_company foreign key (broker_company_id)  references public.broker_company(id);
+
+CREATE TABLE public.company_realtor (
+	id int8 NOT NULL,
+	company_id int8 NOT NULL,
+	realtor_id int8 NOT NULL,
+	primary key (id)
+);
+alter table public.company_realtor add constraint fk_company_realtor_company foreign key (company_id)  references public.company(id);
+alter table public.company_realtor add constraint fk_company_realtor_realtor foreign key (realtor_id)  references public.realtor(id);
+
+CREATE TABLE public.company_realtor_operation (
+	id int8 NOT NULL,
+	company_realtor_id int8 NOT NULL,
+	operation_id int8 NOT NULL,
+	primary key (id)
+);
+
+alter table public.company_realtor_operation add constraint fk_company_realtor_operation_company_realtor foreign key (company_realtor_id)  references public.company_realtor(id);
+alter table public.company_realtor_operation add constraint fk_company_realtor_operation_operation foreign key (operation_id)  references public.operation(id);
+
+CREATE TABLE public.loan_client (
+	id int8 NOT NULL,
+	client_id int8 NOT NULL,
+	loan_id int8 NOT NULL,
+	primary key (id)
+);
+
+alter table public.loan_client add constraint fk_loan_client_loan_officer foreign key (loan_id)  references public.loan_officer(id);
+alter table public.loan_client add constraint fk_loan_client_client foreign key (client_id)  references public.client(id);
+
+
+CREATE TABLE public.loan_client_operation (
+	id int8 NOT NULL,
+	loan_client_id int8 NOT NULL,
+	operation_id int8 NOT NULL,
+	primary key (id)
+);
+
+alter table public.loan_client_operation add constraint fk_loan_client_operation_operation foreign key (operation_id)  references public.operation(id);
+alter table public.loan_client_operation add constraint fk_loan_client_operation_loan_client foreign key (loan_client_id)  references public.loan_client(id);
+
+
+
+
